2daf6894ee32a24b39c6495918b20a535cc0d2f7
Angelica <87237671+Angel-ica@users.noreply.github.com>
2022-11-22 11:26:29 +0000

    Tests scraper

---
+++ test_crypto_compare.py
@@ -0,0 +1,33 @@
+import unittest
+import time
+import os.path 
+from crypto_compare import WebCrawl as wb
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+class TestCryptoPrices(unittest.TestCase):
+    def setUp(self):
+        self.scraper = wb()
+        self.scraper.get_page()
+        self.scraper.view_top_coins()
+        self.scraper.choose_currency()
+        self.scraper.scroll()
+
+    def test_to_csv(self):
+        self.assert_(os.path.isfile('./crypto_compare_data.csv'))
+
+    def test_get_top_coins(self):
+        top_coins={}
+        self.assertEqual(type(top_coins),type(self.scraper.get_top_coins()))
+        self.assertEqual(7,len(self.scraper.get_top_coins()))
+
+    def tearDown(self):
+        self.scraper.driver.quit()
+        
+
+if __name__=='__main__':
+    unittest.main()
+
+    
