c39c2b7deb2913138495f0848887443177c5f137
Angelica <87237671+Angel-ica@users.noreply.github.com>
2022-11-23 12:22:00 +0000

    update scraper

--- coinmarketcap_scraper.py
+++ coinmarketcap_scraper.py
@@ -25,65 +25,91 @@ class WebCrawl():
         self.driver.find_element(by=By.XPATH, value="//div[@class='cmc-cookie-policy-banner__close']").click()
         
     def choose_currency(self):
-        time.sleep(3)
+        time.sleep(2)
         change_currency=self.driver.find_element(by=By.XPATH, value="//div[@class='sc-8580441d-1 klKJWV']//button[@class='sc-476bb07-0 sc-8580441d-7 FerZc']")
         change_currency.click()
         input_preferred_currency=self.driver.find_element(by=By.XPATH, value='//div[@class="sc-785445d2-4 kzsuLu"]//input')
         input_preferred_currency.send_keys('Pound Sterling')
-        time.sleep(3)
+        time.sleep(2)
         preferred_currrency=self.driver.find_element(by=By.XPATH, value='//div[@class="sc-8580441d-3 sc-8580441d-5 hFFJRw"]//div[@class="cmc-currency-picker--icon"]')
         ActionChains(self.driver).move_to_element(preferred_currrency).click().perform()
 
+    def decline_survey(self):
+        time.sleep(2)
+        self.driver.find_element(by=By.XPATH, value='//div[@class="sc-b1f0826a-0 cKSWpr"]//span[2]').click()
+
+    def decline_spot_changes(self):
+        time.sleep(2)
+        close_btn= self.driver.find_element(by=By.XPATH, value="//div[@class='has-style']/*[name()='svg']")
+        ActionChains(self.driver).move_to_element(close_btn).click().perform()
+    def decline_diamonds_and_awards(self):
+        time.sleep(2)
+        self.driver.find_element(by=By.XPATH, value='//div[@class="buttons"]//button[2]').click()
+
+    def decline_sign_up(self):
+        #time.sleep(5)
+        #WebDriverWait(self.driver,20).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="buttons"]//button[2]')))
+        self.driver.find_element(by=By.XPATH, value='//div[@class="buttons"]//button[2]').click()
+
     def scroll(self):
         total_height = int(self.driver.execute_script("return document.body.scrollHeight"))
-        for i in range(1, total_height, 7):
+        for i in range(1, total_height, 9):
             self.driver.execute_script("window.scrollTo(0, {});".format(i))
 
     def get_top_100_coins(self):
-            top_100_coins=self.driver.find_elements(by=By.XPATH, value='//div[@class="sc-853bfcae-1 eibzVK"]//tbody//tr')
-            self.data_dict={'Rank':[],'Time':[],'Coin':[],'Symbol':[],'Price':[],'Change in last 24h':[],'Total vol(24h)':[]}
+        top_100_coins=self.driver.find_elements(by=By.XPATH, value='//div[@class="sc-f7a61dda-2 efhsPu"]//table//tbody//tr')
+        self.data_dict={'Rank':[],'Time':[],'Coin':[],'Price':[],'Change in last 24h':[],'Total vol(24h)':[]}
+        time.sleep(20)
+        for coins in top_100_coins:
+            rank=coins.find_element(by=By.XPATH, value='.//td[3]//div[@class="sc-2f217aa4-3 eyyJUo"]')
+            self.data_dict['Rank'].append(rank.text)
+            scraped_time=time.strftime('%H:%M:%S--%D')
+            self.data_dict['Time'].append(scraped_time)
+            coin=coins.find_element(by=By.XPATH, value='.//td[3]//p')
+            self.data_dict['Coin'].append(coin.text)
+            #symbol=coins.find_element(by=By.XPATH,value= './/td//div[@class="sc-aef7b723-0 LCOyB"]//a//div//div//p')
+            #self.data_dict['Symbol'].append(symbol.text)
+            price=coins.find_element(by=By.XPATH, value='.//td[4]//span')
+            self.data_dict['Price'].append(price.text)
+            assert '£'== price.text[0]
 
-            for coins in top_100_coins:
-                    rank=coins.find_element(by=By.XPATH, value='.//div[@class="sc-6110f1f2-2 iEQXo"]//div')
-                    self.data_dict['Rank'].append(rank.text)
-                    scraped_time=time.strftime('%H:%M:%S--%D')
-                    self.data_dict['Time'].append(scraped_time)
-                    coin=coins.find_element(by=By.XPATH, value='.//td//div[@class="sc-aef7b723-0 LCOyB"]//p[@class="sc-e225a64a-0 ePTNty"]')
-                    self.data_dict['Coin'].append(coin.text)
-                    symbol=coins.find_element(by=By.XPATH,value= './/td//div[@class="sc-aef7b723-0 LCOyB"]//p[@class="sc-e225a64a-0 dfeAJi coin-item-symbol"]')
-                    self.data_dict['Symbol'].append(symbol.text)
-                    price=coins.find_element(by=By.XPATH, value='.//td//div[@class="sc-7510a17-0 hEduBL"]//a//span')
-                    self.data_dict['Price'].append(price.text)
-                    assert '£'==price.text[0]
-                    last_24_hours=coins.find_element(by=By.XPATH, value='.//td//span[@class="sc-97d6d2ca-0 bQjSqS"]')
-                    #caret_up=coins.find_element(by=By.XPATH, value='.//td//span[@class="sc-97d6d2ca-0 cYiHal"]//span')
-                    # caret_down=last_24_hours.find_element(by=By.XPATH, value='.//td//span[@class="sc-97d6d2ca-0 bQjSqS"]//span[@class="icon-Caret-down"]')
-                    # if caret_down in last_24_hours:
-                    #     last_24h='▼',last_24_hours.text
-                    # else:
-                    #     last_24h=('▲',last_24_hours.text)
-                    self.data_dict['Change in last 24h'].append(last_24_hours.text)
-                    #unicode character for the sign is 'U+02D1'
-                    volume_24h_GBP=coins.find_element(by=By.XPATH, value='.//td//div[@class="sc-aef7b723-0 sc-ba1a4d26-0 QisKn"]//a//p')
-                    #volume_24h_coin=coins.find_element(by=By.XPATH, value='.//td//div[@class="sc-aef7b723-0 sc-ba1a4d26-0 QisKn"]//div//p')
-                    self.data_dict['Total vol(24h)'].append(volume_24h_GBP.text)
-                    print(self.data_dict)
-                # except selenium.common.exceptions.NoSuchElementException:
-                #     WebDriverWait(self.driver,20)
+            last_24_hours=coins.find_element(by=By.XPATH, value='.//td[6]//span')
+            attr=last_24_hours.get_attribute('class')
+            if 'cYiHal' in attr:
+                last_24h=('▲'+str(last_24_hours.text))
+                print(last_24h)
+                self.data_dict['Change in last 24h'].append(last_24h)
+            elif 'bQjSqS'  in attr:
+                last_24h=('▼'+str(last_24_hours.text))
+                print(last_24h)
+                self.data_dict['Change in last 24h'].append(last_24h)
 
+            #NOTE UNICODE CHARACTER FOR THE SIGN IS 'U+02D1'
 
+            volume_24h_GBP=coins.find_element(by=By.XPATH, value='.//td//div[@class="sc-aef7b723-0 sc-ba1a4d26-0 QisKn"]//a//p')
+            self.data_dict['Total vol(24h)'].append(volume_24h_GBP.text)
+        print(self.data_dict)
+        return self.data_dict
+                
     def store_in_csv(self):
         df=pd.DataFrame(self.data_dict)
-        df.to_csv("coin_market_cap_data.csv",index=False,header=True)
+        assert not df.empty
+        df.to_csv("coin_marketcap_data.csv",index=False,header=True)
         print('done')
-        time.sleep(2500)
+        time.sleep(20)
+        self.driver.quit()
+        return os.path.isfile('coin_marketcap_data.csv')
         
 def run():
     crawl=WebCrawl()
     crawl.get_page()
+    #crawl.decline_spot_changes()
     crawl.accept_cookies()
     crawl.choose_currency()
     crawl.scroll()
+    #crawl.decline_diamonds_and_awards()
+    #crawl.decline_sign_up()
+    #crawl.decline_survey()
     crawl.get_top_100_coins()
     crawl.store_in_csv()
 
